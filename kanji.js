8b7740c7212d097d762d131794a103a2407704e3
q8cVbWi9IHVk <quezako@gmail.com>
2023-06-09 19:50:57 +0900

    convert format: kanji, kana, furigana

--- kanji.js
+++ kanji.js
@@ -26,7 +26,7 @@ async function dbSearch() {
 
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
-  const strSearch = urlParams.get('kanji')
+  const strSearch = urlParams.get("kanji");
   console.log(strSearch);
 
   const sqlPromise = await initSqlJs({
@@ -42,10 +42,36 @@ async function dbSearch() {
   );
   const result = stmt.getAsObject({});
   strTpl = strBack.replace(/{{FrontSide}}/g, strFront);
-  strTpl = strTpl.replace(/(edit|kanji|kana|furigana):/g, "");
+  strTpl = strTpl.replace(/(edit):/g, "");
   strTpl = strTpl.replace(/{{(#|\/|\^)[^}]+}}/g, "");
-  strTpl = strTpl.replace(/{{([^}]+)}}/gi, function (strSearch, strMatch) {
-    return result[strMatch] ? result[strMatch] : "";
+  strTpl = strTpl.replace(/{{([^}]+)}}/gi, function (strOrig, strMatch) {
+    strReturn = result[strMatch] ? result[strMatch] : "";
+    strMatch = strMatch.replace(
+      /(kanji|kana|furigana):(.+)/g,
+      function (strOrig, strDisplay, strText) {
+        if (strDisplay == "kanji") {
+          strReturn = result[strText]
+            ? result[strText].replace(/\[.+\]/gi, "")
+            : "";
+          console.log(strReturn);
+        } else if (strDisplay == "kana") {
+          strReturn = result[strText]
+            ? result[strText].replace(/.+\[(.+)\]/gi, "$1")
+            : "";
+          console.log(strReturn);
+        } else if (strDisplay == "furigana") {
+          strReturn = result[strText]
+            ? result[strText].replace(
+                /([^ >\[]+)\[([^\]]+)\]/gi,
+                "<ruby>$1<rt>$2</rt></ruby>"
+              )
+            : "";
+          console.log(strReturn);
+        }
+      }
+    );
+
+    return strReturn;
   });
 
   document.querySelector("body").innerHTML = strTpl;
