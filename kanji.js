6c2e2cb60cba7364be1ee83e5ae0b2a2e953a4bf
q8cVbWi9IHVk <quezako@gmail.com>
2023-06-10 00:26:25 +0900

    Auto fetch kanji details

--- kanji.js
+++ kanji.js
@@ -1,9 +1,13 @@
+/**
+ * TODO:
+ * fetch radicals details, from which fields? maximum recursion?
+ * responsive, size: fonts.
+ */
 var strFront = "";
 var strBack = "";
 
 async function dbSearch() {
   var xhr = new XMLHttpRequest();
-
   xhr.open("GET", `../../js/front.tpl`, true);
   xhr.onreadystatechange = function () {
     if (this.readyState !== 4) return;
@@ -19,7 +23,6 @@ async function dbSearch() {
     if (this.status !== 200) return;
     strBack = this.responseText;
   };
-
   xhr.send();
 
   const queryString = window.location.search;
@@ -34,10 +37,10 @@ async function dbSearch() {
   const [SQL, buf] = await Promise.all([sqlPromise, dataPromise]);
   const db = new SQL.Database(new Uint8Array(buf));
 
-  const stmt = db.prepare(
-    `SELECT * FROM Quezako WHERE kanji_only = '${strSearch}' AND (key LIKE '${strSearch}[%' OR key = '${strSearch}}')`
+  var stmt = db.prepare(
+    `SELECT * FROM Quezako WHERE key LIKE '${strSearch}[%' OR key = '${strSearch}}'`
   );
-  const result = stmt.getAsObject({});
+  var result = stmt.getAsObject({});
   strTpl = strBack.replace(/{{FrontSide}}/g, strFront);
   strTpl = strTpl.replace(/(edit):/g, "");
   strTpl = strTpl.replace(/(hint):/g, "");
@@ -99,6 +102,50 @@ async function dbSearch() {
   });
 
   document.querySelector("body").innerHTML = strTpl;
+
+  // Auto fetch kanji details + radical details.
+  const dataPromise2 = fetch("../db/chmn-full.db").then((res) =>
+    res.arrayBuffer()
+  );
+  const [SQL2, buf2] = await Promise.all([sqlPromise, dataPromise2]);
+  const db2 = new SQL.Database(new Uint8Array(buf2));
+
+  strKanjiOnly = strSearch.replace(/[^一-龯々ヶ]/gi, "");
+  strDetails = '<span id="each_details">';
+
+  Array.from(strKanjiOnly).forEach((element) => {
+    strDetails += `<details><summary>${element} details</summary>`;
+
+    stmt = db.prepare(
+      `SELECT mean, chmn_mean, fr_mean_mnemo_wani, fr_story, fr_story_wani_mean, fr_koohii_story_1, fr_koohii_story_2, fr_mean_mnemo_wani2, fr_chmn_mnemo, Tags FROM Quezako WHERE key = "${element}" OR key LIKE "${element}[%"`
+    );
+    result = stmt.getAsObject({});
+    for (var [key, val] of Object.entries(result)) {
+      strDetails += val ? `* ${key}: ${val}<br />` : "";
+    }
+
+    stmt = db.prepare(
+      `SELECT kanji_mnemo_personal FROM Quezako WHERE kanji_mnemo_personal LIKE "%${element} :%"`
+    );
+    result = stmt.getAsObject({});
+    for (var [key, val] of Object.entries(result)) {
+      strDetails += val ? `* ${key}: ${val}<br />` : "";
+    }
+
+    stmt = db2.prepare(
+      `SELECT * FROM \`chmn-full2\` WHERE hanzi = "${element}" OR hanzi2 = "${element}" OR alike = "${element}"`
+    );
+    result = stmt.getAsObject({});
+    for (var [key, val] of Object.entries(result)) {
+      strDetails += val ? `* ${key}: ${val}<br />` : "";
+    }
+    strDetails += "</details>";
+  });
+  strDetails += '</span">';
+
+  document.querySelector("#mnemo_personal").innerHTML += strDetails;
+
+  // Anki script.
   var script = document.createElement("script");
   script.src = "quezako.js";
   document.head.appendChild(script);
